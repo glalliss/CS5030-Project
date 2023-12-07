9840540f7d40653acfaf189f1fc8ffd6d85a7c8b
supriyosadhya <101490016+supriyosadhya@users.noreply.github.com>
2023-12-06 20:43:37 -0700

    Update validate.cpp

--- validate.cpp
+++ validate.cpp
@@ -8,7 +8,7 @@ int main() {
     std::string inputCSV2 = "output_shared_cpu.csv";
     std::string inputCSV3 = "output_shared_gpu.csv";
     std::string inputCSV4 = "output_distributed_cpu.csv";
-    // std::string inputCSV5 = "output_distributed_gpu.csv";
+    std::string inputCSV5 = "output_distributed_gpu.csv";
     // Open the serial CSV file
     std::ifstream file1(inputCSV1);
     if (!file1.is_open()) {
@@ -33,37 +33,37 @@ int main() {
         std::cerr << "Error opening output_distributed_cpu.csv" << std::endl;
         return 1;
     }
-    // // Open the distributed gpu CSV file
-    // std::ifstream file5(inputCSV5);
-    // if (!file5.is_open()) {
-    //     std::cerr << "Error opening output_distributed_gpu.csv" << std::endl;
-    //     return 1;
-    // }
+    // Open the distributed gpu CSV file
+    std::ifstream file5(inputCSV5);
+    if (!file5.is_open()) {
+        std::cerr << "Error opening output_distributed_gpu.csv" << std::endl;
+        return 1;
+    }
     // Read and process data from all files
-    std::string line1, line2, line3, line4;//, line5;
+    std::string line1, line2, line3, line4, line5;
     try
     {
-        while (std::getline(file1, line1) && std::getline(file2, line2) && std::getline(file3, line3) && std::getline(file4, line4)/* && std::getline(file5, line5)*/) {
-            if (line1 != line2 || line1 != line3 || line1 != line4/* || line1 != line5*/)
+        while (std::getline(file1, line1) && std::getline(file2, line2) && std::getline(file3, line3) && std::getline(file4, line4)&& std::getline(file5, line5)) {
+            if (line1 != line2 || line1 != line3 || line1 != line4 || line1 != line5)
             {
                 std::cout << "Output files are not the same" << std::endl;
                 return 1;
             }
-            if (line2 != line3 || line2 != line4/* || line2 != line5*/)
+            if (line2 != line3 || line2 != line4 || line2 != line5)
             {
                 std::cout << "Output files are not the same" << std::endl;
                 return 1;
             }
-            if (line3 != line4/* || line3 != line5*/)
+            if (line3 != line4 || line3 != line5)
             {
                 std::cout << "Output files are not the same" << std::endl;
                 return 1;
             }
-            /*if (line4 != line5)
+            if (line4 != line5)
             {
                 std::cout << "Output files are not the same" << std::endl;
                 return 1;
-            }*/
+            }
         }
     }
     catch(const std::exception& e)
@@ -78,7 +78,7 @@ int main() {
     file2.close();
     file3.close();
     file4.close();
-    // file5.close();
+    file5.close();
     // Finish successfully
     return 0;
 }
